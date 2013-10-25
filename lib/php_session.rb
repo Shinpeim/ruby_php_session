8066a574edc25cfc68e14a6501a556922e3d7263
Shinpei Maruyama <shinpeim@gmail.com>
2013-10-26 04:09:33 +0900

    Make exclusive controll polisy fit to Rack::Session

--- lib/php_session.rb
+++ lib/php_session.rb
@@ -14,60 +14,48 @@ class PHPSession
     }
     @option = default_option.merge(option)
     @session_dir = File.expand_path(session_dir)
-
-    @file = nil
   end
 
   def load(session_id)
-    set_session_id(session_id)
-    @file = File.open(file_path, File::CREAT|File::RDWR)
-
-    unless @file.flock(File::LOCK_EX)
-      raise PHPSession::Errors, "can't obtain lock of session file"
+    with_lock(file_path(session_id)) do |f|
+     # set internal_encoding to nil to avoid encoding conversion
+      f.set_encoding(@option[:external_encoding], nil)
+      Decoder.decode(f.read, @option[:internal_encoding], @option[:encoding_option]) || {}
     end
-
-    # set internal_encoding to nil to avoid encoding conversion
-    @file.set_encoding(@option[:external_encoding], nil)
-    data = Decoder.decode(@file.read, @option[:internal_encoding], @option[:encoding_option]) || {}
-    @file.rewind
-    data
   end
 
-  def loaded?
-    ! @file.nil?
-  end
-
-  def destroy
-    if @file && ! @file.closed?
-      @file.truncate(0)
-    end
-    ensure_file_closed
-    File.delete(file_path)
+  def destroy(session_id)
+    File.delete(file_path(session_id))
   rescue Errno::ENOENT => e
     # file already deleted
   end
 
-  def commit(data)
-    self.load(@session_id) unless @file
-    @file.truncate(0)
-    @file.write(Encoder.encode(data, @option[:external_encoding], @option[:encoding_option]))
-    ensure_file_closed
-  end
-
-  def ensure_file_closed
-    if @file && ! @file.closed?
-      @file.close
+  def save(session_id, data)
+    with_lock(file_path(session_id)) do |f|
+      f.truncate(0)
+      f.write(Encoder.encode(data, @option[:external_encoding], @option[:encoding_option]))
     end
   end
 
   private
+
+  def with_lock(file_path)
+    File.open(file_path, File::CREAT|File::RDWR) do |f|
+      unless f.flock(File::LOCK_EX)
+        raise PHPSession::Errors, "can't obtain lock of session file"
+      end
+      yield(f)
+    end
+  end
 
   def set_session_id(session_id)
     @session_id = session_id
     raise Errors::SecurityError, "directory traversal detected" unless file_path.index(@session_dir) == 0
   end
 
-  def file_path
-    File.expand_path(File.join(@session_dir, "sess_#{@session_id}"))
+  def file_path(session_id)
+    path = File.expand_path(File.join(@session_dir, "sess_#{session_id}"))
+    raise Errors::SecurityError, "directory traversal detected" unless path.index(@session_dir) == 0
+    path
   end
 end
--- spec/php_session_spec.rb
+++ spec/php_session_spec.rb
@@ -2,79 +2,48 @@
 require 'spec_helper'
 
 describe PHPSession do
-  describe "loaded?" do
-    before do
-      @session_file = create_dummy_session_file('key|s:1:"a";')
-    end
-
-    it "should return true when file is loaded" do
-      session = PHPSession.new(@session_file[:dir_name])
-      session.load(@session_file[:session_id])
-      expect(session.loaded?).to be_true
-    end
-
-    it "should return false when file is loaded" do
-      session = PHPSession.new(@session_file[:dir_name])
-      expect(session.loaded?).to be_false
-    end
-
-    after do
-      File.delete(@session_file[:file_path])
-    end
-  end
-
   describe "load" do
-   context "when session file encoding is utf8" do
+    context "when session file encoding is utf8" do
       before do
         @session_file = create_dummy_session_file('key|s:13:"テスト🍺";')
       end
+
       it "should be able to load file with internal:nil, external:utf8" do
         option = {
           :internal_encoding => nil,
           :external_encoding => "UTF-8",
         }
         session = PHPSession.new(@session_file[:dir_name], option)
-        begin
-          data = session.load(@session_file[:session_id])
-          expect(data).to eq({"key" => "テスト🍺"})
-        ensure
-          session.ensure_file_closed
-        end
+        data = session.load(@session_file[:session_id])
+        expect(data).to eq({"key" => "テスト🍺"})
       end
+
       it "should be able to load file with internal:utf8, external:utf8" do
         option = {
           :internal_encoding => "UTF-8",
           :external_encoding => "UTF-8",
         }
         session = PHPSession.new(@session_file[:dir_name], option)
-        begin
-          data = session.load(@session_file[:session_id])
-          expect(data).to eq({"key" => "テスト🍺"})
-        ensure
-          session.ensure_file_closed
-        end
+        data = session.load(@session_file[:session_id])
+        expect(data).to eq({"key" => "テスト🍺"})
       end
+
       it "should return euc-jp string with internal:euc-jp, exterenal:utf8" do
         option = {
           :internal_encoding => "EUC-JP",
           :external_encoding => "UTF-8",
-          :encoding_option => {
-            :undef => :replace
-          }
+          :encoding_option => {:undef => :replace}
         }
         session = PHPSession.new(@session_file[:dir_name], option)
-        begin
-          data = session.load(@session_file[:session_id])
-          expect(data).to eq({"key" => "テスト🍺".encode("EUC-JP", {:undef => :replace})})
-        ensure
-          session.ensure_file_closed
-        end
+        data = session.load(@session_file[:session_id])
+        expect(data).to eq({"key" => "テスト🍺".encode("EUC-JP", {:undef => :replace})})
       end
 
       after do
         File.delete(@session_file[:file_path])
       end
     end
+
     context "when session file exists" do
       before do
         @session_file = create_dummy_session_file('key|s:1:"a";')
@@ -82,12 +51,8 @@ describe PHPSession do
 
       it "should return session data" do
         session = PHPSession.new(@session_file[:dir_name])
-        begin
-          data = session.load(@session_file[:session_id])
-          expect(data).to eq({"key" => "a"})
-        ensure
-          session.ensure_file_closed
-        end
+        data = session.load(@session_file[:session_id])
+        expect(data).to eq({"key" => "a"})
       end
 
       after do
@@ -98,17 +63,13 @@ describe PHPSession do
     context "when session file dosen't exist" do
       it "should return new session data" do
         session = PHPSession.new(Dir.tmpdir)
-        begin
-          data = session.load("session_id")
-          expect(data).to eq({})
-        ensure
-          session.ensure_file_closed
-        end
+        data = session.load("session_id")
+        expect(data).to eq({})
       end
     end
   end
 
-  describe "#commit" do
+  describe "#save" do
     before do
       @session_file = create_dummy_session_file('key|s:1:"a";')
     end
@@ -122,18 +83,11 @@ describe PHPSession do
       session = PHPSession.new(@session_file[:dir_name], option)
       data = session.load(@session_file[:session_id])
       data["key"] = "テスト🍣"
-      session.commit(data)
+      session.save(@session_file[:session_id], data)
 
       # read in bytesequence mode to avoid encoding conversion
       byte_sequence = IO.read(@session_file[:file_path], File.size(@session_file[:file_path]))
       expect(byte_sequence.force_encoding('EUC-JP')).to eq('key|s:7:"テスト?";'.encode("EUC-JP"))
-    end
-
-    it "should save session data even if session_file is not loaded" do
-      session = PHPSession.new(@session_file[:dir_name])
-      expect {
-        session.commit({:hoge => "nyan"})
-      }.not_to raise_error
     end
 
     after do
@@ -145,12 +99,11 @@ describe PHPSession do
     context "when session file exists and loaded" do
       before do
         @session_file = create_dummy_session_file('key|s:1:"a";')
-        @session = PHPSession.new(@session_file[:dir_name])
-        @session.load(@session_file[:session_id])
       end
 
       it "should delete session file" do
-        @session.destroy
+        session = PHPSession.new(@session_file[:dir_name])
+        session.destroy(@session_file[:session_id])
         expect(File.exists?(@session_file[:file_path])).to eq(false)
       end
     end
